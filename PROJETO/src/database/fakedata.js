3eebb1837c5fccce697062202349043677b154fb
AmatiF <54911731+CoreosG@users.noreply.github.com>
2020-08-11 12:10:10 -0300

    PROJETO CONCLUÍDO!
    obrigado Rockeatseat!

---
+++ PROJETO/src/database/fakedata.js
@@ -0,0 +1,24 @@
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "13997204558", 
+        bio: "Entusiasta das melhores tecnologias da química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20,00", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Amati Francisco", 
+        avatar: "https://avatars0.githubusercontent.com/u/54911731?s=460&u=c456d4af2cd22d85f817ad2b0bfd8b9d2fe442dd&v=4", 
+        whatsapp: "13997204558", 
+        bio: "Entusiasta das melhores tecnologias da química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20,00", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    }
+]
---
+++ PROJETO/src/server.js
@@ -0,0 +1,30 @@
+// servidor
+const express = require('express')
+const server = express()
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+// Configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+// Iniciar a configuração do servidor
+server
+// Receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+// configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+// rotas de aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+// start do servidor
+.listen(5500)
